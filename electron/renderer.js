65f62b3bb375eeab8fe2a88084cf804bbb6eb752
alexandrebignalet <alexandre.bignalet@viacesi.fr>
2017-05-22 19:42:36 -0400

    removing outdated files

--- electron/renderer.js
+++
@@ -1,6 +0,0 @@
-const Repository = require('../database/repository/repository');
-const Production = require('../database/domain/production');
-
-window.repositories = {
-    production: new Repository('productions', Production)
-};

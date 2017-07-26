3ba61d297f322ddcfe55e8edaf92ac1f5f575549
alexandrebignalet <alexandre.bignalet@viacesi.fr>
2017-07-26 14:57:49 +0200

    Fixes #31 form production updated

--- app/components/management/facility_charges/facility_charges_form.component.js
+++ app/components/management/facility_charges/facility_charges_form.component.js
@@ -17,26 +17,54 @@ export const FacilityChargesFormComponent = {
         }
 
         $onInit() {
-            this.facilityCharges = this.resolve.facilityCharges;
+            this.facilityCharges = this.convertToShow(this.resolve.facilityCharges);
+        }
+
+        convertToShow(facilityCharges) {
+            facilityCharges.chickPrice *= 1000;
+            facilityCharges.warming *= 1000;
+            facilityCharges.vetPrice *= 1000;
+            facilityCharges.contributions *= 1000;
+            facilityCharges.disinfection *= 1000;
+            facilityCharges.litter *= 1000;
+            facilityCharges.commodities *= 1000;
+            facilityCharges.catching *= 1000;
+            facilityCharges.insurances *= 1000;
+            return facilityCharges;
+        }
+
+        convertToSave(facilityCharges) {
+            facilityCharges.chickPrice /= 1000;
+            facilityCharges.warming /= 1000;
+            facilityCharges.vetPrice /= 1000;
+            facilityCharges.contributions /= 1000;
+            facilityCharges.disinfection /= 1000;
+            facilityCharges.litter /= 1000;
+            facilityCharges.commodities /= 1000;
+            facilityCharges.catching /= 1000;
+            facilityCharges.insurances /= 1000;
+            return facilityCharges;
         }
 
         onSubmit() {
             this.isSaving = true;
+            let facilityCharges = this.convertToSave(this.facilityCharges);
+
             switch(this.currentState.replace("facility_charges.", "")) {
                 case 'edit':
-                    this.dataService.update(this.facilityCharges).then(() => {
+                    this.dataService.update(facilityCharges).then(() => {
                         this.toastr.success('a été mise à jour.', this.facilityCharges.name);
                         this.modalInstance.close()
                     });
                     break;
                 case 'remove':
-                    this.dataService.remove(this.facilityCharges).then(() => {
+                    this.dataService.remove(facilityCharges).then(() => {
                         this.toastr.warning('a été supprimée.', this.facilityCharges.name);
                         this.modalInstance.close()
                     });
                     break;
                 case 'create':
-                    this.dataService.create(this.facilityCharges).then(() => {
+                    this.dataService.create(facilityCharges).then(() => {
                         this.toastr.info('a été créée.', this.facilityCharges.name);
                         this.modalInstance.close()
                     });

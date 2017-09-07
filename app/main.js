0902b53b9c210b05abd8247f419046e447bea342
alexandrebignalet <alexandre.bignalet@viacesi.fr>
2017-09-07 13:09:34 +0200

    sqlite3 issue

---
+++ app/main.js
@@ -0,0 +1,73 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+const electron = require('electron');
+const {shell} = require('electron');
+
+const fs = require('fs');
+const os = require('os');
+const ipc = electron.ipcMain;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow () {
+
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600, webPreferences: { nodeIntegration: true }});
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.resolve(__dirname, 'webapp/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.maximize();
+    // Open the DevTools.
+    // win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    app.quit();
+});
+
+ipc.on('print-to-pdf', (event) => {
+    const pdfPath = path.join(os.tmpdir(), 'print.pdf');
+    console.log(pdfPath);
+    const win = BrowserWindow.fromWebContents(event.sender);
+
+    win.webContents.printToPDF({}, (err, data) => {
+        if(err) return console.log(err.message);
+
+        fs.writeFile(pdfPath, data, (err) => {
+            if(err) return console.log(err.message);
+            shell.openExternal('file://' + pdfPath);
+            event.sender.send('wrote-pdf', pdfPath);
+        })
+    })
+});
+
+app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (win === null) {
+        createWindow();
+    }
+});

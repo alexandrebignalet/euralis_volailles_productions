f2fbc3b0bc4405cda09a524f44b129e2863aac0d
alexandrebignalet <alexandre.bignalet@gmail.com>
2020-03-15 08:37:19 +0100

    add webpack bundle analyzer and fix security issue upgradind jquery

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,10 +1,14 @@
 import path    from 'path';
 import {config}  from './webpack.config';
 import webpack from 'webpack';
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 config.devtool = 'source-map';
 config.output.path = path.resolve(__dirname, './');
-config.plugins = config.plugins.concat([new webpack.HotModuleReplacementPlugin()]);
+config.plugins = config.plugins.concat([
+  new webpack.HotModuleReplacementPlugin(),
+  new BundleAnalyzerPlugin()
+]);
 config.entry.renderer = ['webpack-hot-middleware/client?reload=true'].concat(config.entry.renderer);
 
 module.exports = config;
